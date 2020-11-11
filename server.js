8db9557da3e5233d669b51eceb2b0185f8a35f8d
Myles Burt <myles_burt@rocketmail.com>
2020-11-12 01:06:28 +1030

    Creating and fine tuning the functions to add departments, roles and employees. Connection to the server is still not working.

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
     host: "localhost",
-    port: 4000,
+    port: 4400,
     user: "root",
-    password: ".......",
+    password: "#codingismypassion20",
     database: "employee_trackerDB"
 });
 
@@ -16,21 +16,20 @@ connection.connect(function (err) {
 });
 
 function runTracker() {
-    inquirer
-        .prompt({
-            name: "trackerOptions",
-            type: "rawlist",
-            message: "Welcome to Employee Tracker, how may I help you?",
-            choices: [
-                "Add department",
-                "Add role",
-                "Add employee",
-                "View departments",
-                "View roles",
-                "View employees",
-                "Update employee roles"
-            ]
-        })
+    inquirer.prompt({
+        name: "trackerOptions",
+        type: "rawlist",
+        message: "Welcome to Employee Tracker, how may I help you?",
+        choices: [
+            "Add department",
+            "Add role",
+            "Add employee",
+            "View departments",
+            "View roles",
+            "View employees",
+            "Update employee roles"
+        ]
+    })
         .then(function (answer) {
             switch (answer.trackerOptions) {
                 case "Add department":
@@ -61,5 +60,118 @@ function runTracker() {
                     updateEmployees();
                     break;
             }
-        })
+        });
+}
+
+// This function will add user data into the department table of schema.sql
+
+function addDepartment() {
+    inquirer.prompt([
+        {
+            name: "departmentAdded",
+            type: "input",
+            message: "What department would you like to add?"
+        }
+    ])
+        .then(function (answer) {
+            connection.query("INSERT...",
+                [
+                    {
+                        department_name: answer.departmentAdded
+                    }
+                ],
+                function (err) {
+                    if (err) throw err;
+                    console.log("Role " + answer.departmentAdded + " has been added.");
+                    runTracker();
+                }
+            );
+        });
+}
+
+// This function will add user data into the employee_role table of schema.sql
+
+function addRole() {
+    inquirer.prompt([
+        {
+            name: "roleAdded",
+            type: "input",
+            message: "What role would you like to add?"
+        },
+        {
+            name: "roleSalary",
+            type: "input",
+            message: "What is the salary for this role?"
+        },
+        {
+            name: "roleDepartment",
+            type: "rawlist",
+            choices: function () {
+                let roleDepartArray = [];
+                for (var i = 0; i < results.length; i++) {
+                    roleDepartArray.push(results[i].department_name);
+                }
+                return roleDepartArray;
+            },
+            message: "What department does this role belong to?"
+        }
+    ])
+        .then(function (answer) {
+            connection.query("INSERT...",
+                [
+                    {
+                        role_title: answer.roleAdded,
+                    },
+                    {
+                        role_salary: answer.roleSalary
+                    }
+                ],
+                function (err) {
+                    if (err) throw err;
+                    console.log("Role " + answer.roleAdded + " has been added.");
+                    runTracker();
+                }
+            );
+        });
+}
+
+// This function will add user data into the employee table of schema.sql
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            name: "employeeFirst",
+            type: "input",
+            message: "What is the Employee's first name?"
+        },
+        {
+            name: "employeeLast",
+            type: "input",
+            message: "What it the Employee's last name?"
+        },
+        {
+            name: "employeeRole",
+            type: "rawlist",
+            choices: function () {
+                let roleArray = [];
+                for (var i = 0; i < results.length; i++) {
+                    roleArray.push(results[i].role_title);
+                }
+                return roleArray;
+            },
+            message: "What is the role of the Employee?",
+        }
+    ])
+        .then(function (answer) {
+            connection.query("INSERT...",
+                {
+                    employeeAdded: answer.employeeAdded
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("Role " + answer.employeeAdded + " has been added.");
+                    runTracker();
+                }
+            );
+        });
 }
