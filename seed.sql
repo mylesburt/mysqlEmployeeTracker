aaba02142a0ab27bc6839a89d85ec055b8f2cc0e
Myles Burt <myles_burt@rocketmail.com>
2020-11-12 02:22:12 +1030

    Inserts filled for the employee_trackerDB through the seed file.

---
+++ seed.sql
@@ -0,0 +1,26 @@
+USE employee_trackerDB;
+
+INSERT INTO department (department_name, department_id)
+VALUES ("Editorial", 1), ("Markerting", 2), ("Printing", 3), ("Front Office", 4);
+
+INSERT INTO employee_role (role_title, role_salary, role_id)
+VALUES ("Journalist", 45000, 1), ("Salesperson", 75000, 2), ("Printer", 50000, 3), ("Receptionist", 60000, 4);
+
+INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
+VALUES ("Bob", "McGee", 1);
+INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
+VALUES ("Beth", "Greenwood", 2);
+INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
+VALUES ("Sampson", "Butler", 3);
+INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
+VALUES ("Doris", "Harring", 4);
+INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
+VALUES ("Tracey", "Spalding", 5);
+INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
+VALUES ("Frank", "Bickford", 6);
+INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
+VALUES ("Tiffany", "Bloomenfold", 7);
+INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
+VALUES ("Jenny", "Swifton", 8);
+INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
+VALUES ("Troy", "Moran", 9);
