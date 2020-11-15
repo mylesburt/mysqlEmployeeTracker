06cc56b556e35b422465c3994b7171dc44b2ed00
Myles Burt <myles_burt@rocketmail.com>
2020-11-15 12:00:15 +1030

    Proper mysql seed layout added.

--- seed.sql
+++ seed.sql
@@ -1,26 +1,25 @@
 USE employee_trackerDB;
 
-INSERT INTO department (department_name, department_id)
-VALUES ("Editorial", 1), ("Markerting", 2), ("Printing", 3), ("Front Office", 4);
-
-INSERT INTO employee_role (role_title, role_salary, role_id)
-VALUES ("Journalist", 45000, 1), ("Salesperson", 75000, 2), ("Printer", 50000, 3), ("Receptionist", 60000, 4);
-
-INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
-VALUES ("Bob", "McGee", 1);
-INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
-VALUES ("Beth", "Greenwood", 2);
-INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
-VALUES ("Sampson", "Butler", 3);
-INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
-VALUES ("Doris", "Harring", 4);
-INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
-VALUES ("Tracey", "Spalding", 5);
-INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
-VALUES ("Frank", "Bickford", 6);
-INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
-VALUES ("Tiffany", "Bloomenfold", 7);
-INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
-VALUES ("Jenny", "Swifton", 8);
-INSERT INTO employee (employee_first_name, employee_last_name, employee_id)
-VALUES ("Troy", "Moran", 9);
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
